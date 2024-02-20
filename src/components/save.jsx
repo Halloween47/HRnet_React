7676e1b937f12038672fdfee5b0c41b5c9aa6f17
Thomas Leconte <thomasl.dw20@gmail.com>
2024-02-20 17:04:00 +0100

    Probleme librairie MODAL

--- src/components/save.jsx
+++ src/components/save.jsx
@@ -2,34 +2,12 @@ import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addEmployee } from '../store/employees'
 
-// import Modal from './modal'
-// import Modal from 'my-modal-plugin'
-import { Modal } from 'wealth-health-modal';
-// import  Modal  from 'wealth-health-modal';
+// import Modal from 'my-modal-wh'
+
 
 function Save() {
-  //     // const [firstName, setFirstname] = useState('');
-
-  // useEffect(() => {
-  //     const firstNameID = document.getElementById('first-name');
-  //     // console.log(firstName);
-  //     if (firstNameID && typeof firstNameID.value === 'string' && firstNameID.value.trim() !== '') {
-  //         console.log('La propriété "value" est définie pour cet élément HTML.');
-  //         // setFirstname(firstNameID.value);
-  //     } else {
-  //         console.log('La propriété "value" n\'est pas définie pour cet élément HTML.');
-  //     }
-  // }, []);
-
-  // console.log(firstName);
-  // const [lastName, setLastname] = useState('')
-  const [dateOfBirthFormat, setDateOfBirthFormat] = useState('dd MMMM yyyy') // Format de date par défaut
-  const [startDateFormat, setStartDateFormat] = useState('dd MMMM yyyy') // Format de date par défaut
-  // const [street, setStreet] = useState('')
-  // const [city, setCity] = useState('')
-  // const [state, setState] = useState('')
-  // const [zipCode, setZipCode] = useState('')
-  // const [departement, setDepartement] = useState('')
+  // const [dateOfBirthFormat, setDateOfBirthFormat] = useState('dd MMMM yyyy') 
+  // const [startDateFormat, setStartDateFormat] = useState('dd MMMM yyyy') 
 
   const [showModal, setShowModal] = useState(false)
   const [etatModal, setEtatModal] = useState(null)
@@ -82,9 +60,9 @@ function Save() {
     }
   }
 
-  const closeModal = () => {
-    setShowModal(false)
-  }
+  // const closeModal = () => {
+  //   setShowModal(false)
+  // }
 
   return (
     <>
@@ -92,7 +70,7 @@ function Save() {
         Save
       </button>
       {/* {showModal && <Modal onClose={closeModal} etat={etatModal} />} */}
-      <Modal />
+      
     </>
   )
 }
---
+++ src/my-modal-wh.d.ts
@@ -0,0 +1,1 @@
+declare module 'my-modal-wh';
