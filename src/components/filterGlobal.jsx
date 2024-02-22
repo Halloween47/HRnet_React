1e99b81c82c875a83608fe61c4314a05f4fc07c3
Thomas Leconte <thomasl.dw20@gmail.com>
2024-02-23 00:06:20 +0100

    Filtre Prenom OK

--- src/components/filterGlobal.jsx
+++ src/components/filterGlobal.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import FilterComponent from './filterComponent';
+import { dataMocked } from '../datas/datasMocked';
 
-export const GlobalFilter = ({ filter, setFilter }) => {
-  return (
-    <span>
-      Recherche :{' '}
-      <input value={filter || ''} onchange={(e) => setFilter(e.target.value)} />
-    </span>
-  )
-}
+
+
+
+export const GlobalFilter = () => {
+	const [filterText, setFilterText] = React.useState('');
+	const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
+	const filteredItems = dataMocked.filter(
+		item => item.name && item.name.toLowerCase().includes(filterText.toLowerCase()),
+	);
+
+	const subHeaderComponentMemo = React.useMemo(() => {
+		const handleClear = () => {
+			if (filterText) {
+				setResetPaginationToggle(!resetPaginationToggle);
+				setFilterText('');
+			}
+		};
+
+		return (
+			<FilterComponent onFilter={e => setFilterText(e.target.value)} onClear={handleClear} filterText={filterText} />
+		);
+	}, [filterText, resetPaginationToggle]);
+
+};
+export default GlobalFilter;
